ed1a1f9201c6d63e43bca310f457cfff5a4eea2d
Alawode Adewale Afeez <eyesightconcept@gmail.com>
2026-04-17 05:22:01 -0700

    fix: remove global auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 
 import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
@@ -8,9 +7,6 @@ import { PharmacyModule } from './modules/pharmacy/pharmacy.module';
 import { AutomobileModule } from './modules/automobile/automobile.module';
 import { RealEstateModule } from './modules/real-estate/real-estate.module';
 import { CloudinaryModule } from './modules/cloudinary/cloudinary.module';
-
-import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
-import { RolesGuard } from './modules/auth/guards/roles.guard';
 
 @Module({
   imports: [
@@ -21,10 +17,6 @@ import { RolesGuard } from './modules/auth/guards/roles.guard';
     AutomobileModule,
     RealEstateModule,
     CloudinaryModule,
-  ],
-  providers: [
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
   ],
 })
 export class AppModule {}
